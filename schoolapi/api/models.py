afd5f450062426b762ac2fd92a07ec2a315b8467
lateo16 <lateofrederick@gmail.com>
2020-07-26 10:55:48 +0000

    created the models for the api

--- schoolapi/api/models.py
+++ schoolapi/api/models.py
@@ -1,3 +1,66 @@
 from django.db import models
 
-# Create your models here.
+
+class Guardian(models.Model):
+    name = models.CharField(max_length=60, null=False)
+    telephone = models.CharField(max_length=10)
+    location = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
+
+
+class Courses(models.Model):
+    name = models.CharField(max_length=40, null=False)
+    courseCode = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=40, null=False)
+    # head = models.ForeignKey(
+    #     to=Lecturer,
+    #     on_delete=models.CASCADE
+    # )
+    totalStudents = models.IntegerField()
+    totalLecturers = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Student(models.Model):
+    firstName = models.CharField(max_length=25, null=False)
+    lastName = models.CharField(max_length=25, null=False)
+    yearOfBirth = models.DateField()
+    currentLevel = models.IntegerField()
+    department = models.ForeignKey(
+        to=Department,
+        on_delete=models.CASCADE
+    )
+    guardian = models.ForeignKey(
+        to=Guardian,
+        on_delete=models.CASCADE,
+        related_name="students"
+    )
+    coursesRegistered = models.ManyToManyField(
+        to=Courses
+    )
+
+    def __str__(self):
+        return self.firstName
+
+
+class Lecturer(models.Model):
+    name = models.CharField(max_length=40, null=False)
+    department = models.ForeignKey(
+        to=Department,
+        on_delete=models.CASCADE
+    )
+    course = models.ForeignKey(
+        to=Courses,
+        on_delete=models.CASCADE
+    )
+
