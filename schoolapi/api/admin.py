0d15d285292b91ade165a878bc7427ad8f94fceb
lateo16 <lateofrederick@gmail.com>
2020-07-26 10:56:47 +0000

    registered the models to be visible in admin site

--- schoolapi/api/admin.py
+++ schoolapi/api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Guardian, Student, Courses, Lecturer, Department
 
 # Register your models here.
+admin.site.register(Guardian)
+admin.site.register(Student)
+admin.site.register(Courses)
+admin.site.register(Lecturer)
+admin.site.register(Department)
+
